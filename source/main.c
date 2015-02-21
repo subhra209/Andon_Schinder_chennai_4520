c98cc997ec12b7b570a760659f42f808decba366
RavikumarGowda <rkb502@gmail.com>
2015-02-21 15:43:50 +0530

    Tested

---
+++ source/main.c
@@ -0,0 +1,214 @@
+
+/*
+*------------------------------------------------------------------------------
+* main.c
+*
+*------------------------------------------------------------------------------
+*/
+
+
+
+/*
+*------------------------------------------------------------------------------
+* Include Files
+*------------------------------------------------------------------------------
+*/
+
+#include <timers.h>				// Timer library functions
+#include <delays.h>				// Delay library functions
+#include <string.h>				// String library functions
+#include "board.h"				// board setup
+#include "timer.h"
+#include "heartbeat.h"
+#include "uart_driver.h"
+#include "communication.h"
+#include "app.h"
+#include "ui.h"
+#include "lcd.h"
+#include "keypad_driver.h"
+/*
+*------------------------------------------------------------------------------
+* Private Defines
+*------------------------------------------------------------------------------
+*/
+
+/*
+*------------------------------------------------------------------------------
+* Processor config bits
+*------------------------------------------------------------------------------
+*/
+
+#pragma config OSC      = INTIO67
+#pragma config FCMEN    = OFF
+#pragma config IESO     = OFF
+#pragma config PWRT     = OFF
+#pragma config BOREN    = ON
+#pragma config BORV     = 3
+#pragma config WDT      = OFF
+#pragma config WDTPS    = 512	
+#pragma config MCLRE    = ON
+#pragma config LPT1OSC  = OFF
+#pragma config PBADEN   = OFF
+#pragma config STVREN   = ON
+#pragma config LVP      = OFF
+//#pragma config ICPRT  = OFF       // Dedicated In-Circuit Debug/Programming
+#pragma config XINST    = OFF       // Extended Instruction Set
+#pragma config CP0      = OFF
+#pragma config CP1      = OFF
+#pragma config CP2      = OFF
+#pragma config CP3      = OFF
+#pragma config CPB      = OFF
+#pragma config CPD      = OFF
+#pragma config WRT0     = OFF
+#pragma config WRT1     = OFF
+#pragma config WRT2     = OFF
+//#pragma config WRT3   = OFF
+#pragma config WRTB     = OFF//N       // Boot Block Write Protection
+#pragma config WRTC     = OFF
+#pragma config WRTD     = OFF
+#pragma config EBTR0    = OFF
+#pragma config EBTR1    = OFF
+#pragma config EBTR2    = OFF
+#pragma config EBTR3    = OFF
+#pragma config EBTRB    = OFF
+/*
+*------------------------------------------------------------------------------
+* Private Macros
+*------------------------------------------------------------------------------
+*/
+
+/*
+*------------------------------------------------------------------------------
+* Private Data Types
+*------------------------------------------------------------------------------
+*/
+
+/*
+*------------------------------------------------------------------------------
+* Public Variables
+*------------------------------------------------------------------------------
+*/
+
+/*
+*------------------------------------------------------------------------------
+* Private Variables (static)
+*------------------------------------------------------------------------------
+*/
+UINT8 message[]="IDEONICS";
+
+/*
+*------------------------------------------------------------------------------
+* Public Constants
+*------------------------------------------------------------------------------
+*/
+
+/*
+*------------------------------------------------------------------------------
+* Private Constants (static)
+*------------------------------------------------------------------------------
+*/
+
+/*
+*------------------------------------------------------------------------------
+* Private Function Prototypes (static)
+*------------------------------------------------------------------------------
+*/
+
+/*
+*------------------------------------------------------------------------------
+* Public Functions
+*------------------------------------------------------------------------------
+*/
+
+/*
+*------------------------------------------------------------------------------
+* Private Functions
+*------------------------------------------------------------------------------
+*/
+
+/*
+*------------------------------------------------------------------------------
+* void main(void)
+
+* Summary	: Application specifc main routine. Initializes all port and
+*			: pheriperal and put the main task into an infinite loop.
+*
+* Input		: None
+*
+* Output	: None
+*
+*------------------------------------------------------------------------------
+*/
+
+#define TICK_PERIOD	(65535 - 20000)  // 2.5ms
+
+
+
+
+void main(void)
+{
+	UINT8 i;
+	
+
+
+	BRD_init( );			//board initialization
+
+	LCD_init();
+	InitializeKeypad();
+
+	TIMER0_init(TICK_PERIOD,0);							//initialize timer0
+
+#ifdef TEST_LCD
+	for(i = 0 ; i< 26; i++)
+	{
+		LCD_putChar(i+'A');
+		DelayMs(200);
+	}
+	LCD_clear();
+#endif
+
+
+	UI_init();			//MUST BE DONE AFTER IAS INIT
+
+	APP_init();
+
+	EnableInterrupts();
+
+
+   	while(TRUE)
+    {
+
+
+		if( heartBeatCount >= 200)
+		{
+			HB_task();
+			heartBeatCount = 0;
+		}
+
+		if( keypadUpdateCount >= 8 )
+		{
+			UpdateKeypadTask();
+			keypadUpdateCount = 0;
+		}
+
+		COM_task();
+
+
+		APP_task();
+	
+	
+		UI_task();
+	
+	
+
+
+		ClrWdt();
+				
+    }	 
+				
+   
+}
+
+/*
+*  End of main.c
+*/
